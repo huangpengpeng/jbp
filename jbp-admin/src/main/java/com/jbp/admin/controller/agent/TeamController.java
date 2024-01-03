0a07be43f0544508ade5f77385dbfcb27f9a34c4
nie meng yong <3083131693@qq.com>
2024-01-03 11:39:10 +0800

    1

--- jbp-admin/src/main/java/com/jbp/admin/controller/agent/TeamController.java
+++ jbp-admin/src/main/java/com/jbp/admin/controller/agent/TeamController.java
@@ -10,22 +10,26 @@ import com.jbp.common.result.CommonResult;
 import com.jbp.service.service.TeamService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("api/admin/agent/team")
-@Api(tags = "团队管理")
+@Api(tags = "团队管理积分")
 public class TeamController {
     @Resource
     private TeamService teamService;
 
+    @PreAuthorize("hasAuthority('agent:team:page')")
     @GetMapping("/page")
     @ApiOperation("列表分页查询")
     public CommonResult<CommonPage<Team>> getList(TeamPageRequest request, PageParamRequest pageParamRequest) {
         return CommonResult.success(CommonPage.restPage(teamService.pageList(request.getName(), pageParamRequest)));
     }
+
+    @PreAuthorize("hasAuthority('agent:team:add')")
     @PostMapping("/add")
     @ApiOperation("新增")
     public CommonResult add(@RequestBody TeamRequest request) {
@@ -33,6 +37,7 @@ public class TeamController {
         return CommonResult.success();
     }
 
+    @PreAuthorize("hasAuthority('agent:team:update')")
     @PostMapping("/update")
     @ApiOperation("修改")
     public CommonResult update(@RequestBody TeamEditRequest request) {
@@ -40,6 +45,7 @@ public class TeamController {
         return CommonResult.success();
     }
 
+    @PreAuthorize("hasAuthority('agent:team:delete')")
     @GetMapping("/delete/{id}")
     @ApiOperation("删除")
     public CommonResult delete(@PathVariable("id") Integer id) {
