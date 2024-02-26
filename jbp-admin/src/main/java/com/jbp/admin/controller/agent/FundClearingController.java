be140f8054853e96ebf0a4de1b59bd311d7f8404
nie meng yong <3083131693@qq.com>
2024-02-26 14:31:42 +0800

    1

--- jbp-admin/src/main/java/com/jbp/admin/controller/agent/FundClearingController.java
+++ jbp-admin/src/main/java/com/jbp/admin/controller/agent/FundClearingController.java
@@ -3,20 +3,16 @@ package com.jbp.admin.controller.agent;
 import cn.hutool.core.collection.CollUtil;
 import com.jbp.common.model.agent.FundClearing;
 import com.jbp.common.page.CommonPage;
-import com.jbp.common.request.OrderSearchRequest;
 import com.jbp.common.request.PageParamRequest;
 import com.jbp.common.request.agent.FundClearingRequest;
+import com.jbp.common.request.agent.FundClearingUpdateRequest;
 import com.jbp.common.result.CommonResult;
 import com.jbp.service.service.agent.FundClearingService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -42,10 +38,50 @@ public class FundClearingController {
         return CommonResult.success(FundClearing.Constants.values());
     }
 
+    @PreAuthorize("hasAuthority('agent:fund:clearing:update:wait:audit')")
+    @PostMapping("/update/wait/audit")
+    @ApiOperation("更新待审核")
+    public CommonResult updateWaitAudit(@RequestBody FundClearingUpdateRequest request) {
+        fundClearingService.updateWaitAudit(request.getIds(), request.getRemark());
+        return CommonResult.success();
+    }
+
+    @PreAuthorize("hasAuthority('agent:fund:clearing:update:wait:send')")
+    @PostMapping("/update/wait/send")
+    @ApiOperation("更新待出款")
+    public CommonResult updateWaitSend(@RequestBody FundClearingUpdateRequest request) {
+        fundClearingService.updateWaitSend(request.getIds(), request.getRemark());
+        return CommonResult.success();
+    }
+
+    @PreAuthorize("hasAuthority('agent:fund:clearing:update:send')")
+    @PostMapping("/update/send")
+    @ApiOperation("更新已出款")
+    public CommonResult updateSend(@RequestBody FundClearingUpdateRequest request) {
+        fundClearingService.updateSend(request.getIds(), request.getRemark());
+        return CommonResult.success();
+    }
+
+    @PreAuthorize("hasAuthority('agent:fund:clearing:update:cancel')")
+    @PostMapping("/update/cancel")
+    @ApiOperation("更新已取消")
+    public CommonResult updateCancel(@RequestBody FundClearingUpdateRequest request) {
+        fundClearingService.updateCancel(request.getIds(), request.getRemark());
+        return CommonResult.success();
+    }
+
+    @PreAuthorize("hasAuthority('agent:fund:clearing:update:intercept')")
+    @PostMapping("/update/intercept")
+    @ApiOperation("更新已拦截")
+    public CommonResult updateIntercept(@RequestBody FundClearingUpdateRequest request) {
+        fundClearingService.updateIntercept(request.getIds(), request.getRemark());
+        return CommonResult.success();
+    }
+
     @PreAuthorize("hasAuthority('agent:fund:clearing:excel')")
     @ApiOperation(value = "佣金发放记录导出Excel")
     @RequestMapping(value = "/excel", method = RequestMethod.GET)
-    public CommonResult<HashMap<String, String>> exportOrder(FundClearingRequest request){
+    public CommonResult<HashMap<String, String>> exportOrder(FundClearingRequest request) {
         String fileName = fundClearingService.exportOrder(request.getUniqueNo(), request.getExternalNo(), request.getStartClearingTime(), request.getEndClearingTime(), request.getStartCreateTime(), request.getEndCreateTime(), request.getStatus());
         HashMap<String, String> map = CollUtil.newHashMap();
         map.put("fileName", fileName);
