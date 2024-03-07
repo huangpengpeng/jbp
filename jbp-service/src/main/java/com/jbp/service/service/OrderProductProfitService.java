fe9cb4ae0a0aa4967c973772d2e4ebea3c20cde4
nie meng yong <3083131693@qq.com>
2024-03-07 14:56:58 +0800

    1

--- jbp-service/src/main/java/com/jbp/service/service/OrderProductProfitService.java
+++ jbp-service/src/main/java/com/jbp/service/service/OrderProductProfitService.java
@@ -1,7 +1,9 @@
 package com.jbp.service.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.pagehelper.PageInfo;
 import com.jbp.common.model.order.OrderProductProfit;
+import com.jbp.common.request.PageParamRequest;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface OrderProductProfitService extends IService<OrderProductProfit> 
     OrderProductProfit save(Integer orderId, String orderNo, Integer productId, Integer profitType, String profitName, String rule);
 
     List<OrderProductProfit> getByOrder(Integer orderId, Integer profitType, String status);
+
+    PageInfo<OrderProductProfit> pageList(String orderNo, String profitName, String status, PageParamRequest pageParamRequest);
 }
