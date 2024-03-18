762b066af520cd6333e527596dd0faa44827acfd
nie meng yong <3083131693@qq.com>
2024-03-18 14:08:01 +0800

    1

--- jbp-service/src/main/java/com/jbp/service/dao/agent/WalletWithdrawDao.java
+++ jbp-service/src/main/java/com/jbp/service/dao/agent/WalletWithdrawDao.java
@@ -3,6 +3,13 @@ package com.jbp.service.dao.agent;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jbp.common.model.agent.WalletFlow;
 import com.jbp.common.model.agent.WalletWithdraw;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.security.core.parameters.P;
+
+import java.util.List;
 
 public interface WalletWithdrawDao extends BaseMapper<WalletWithdraw> {
+    List<WalletWithdraw> pageList(@Param("account") String account,@Param("walletName") String walletName,@Param("status") String status,
+                                 @Param("endTime") String endTime,@Param("startTime")  String startTime,@Param("realName") String realName,@Param("channelName") String channelName);
+
 }
