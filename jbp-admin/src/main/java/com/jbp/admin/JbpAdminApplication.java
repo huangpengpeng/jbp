20088d37e6f99b76a9939050d4730ba6622b091a
fanyufengkaiying <956838209@qq.com>
2024-03-26 21:51:01 +0800

    1

--- jbp-admin/src/main/java/com/jbp/admin/JbpAdminApplication.java
+++ jbp-admin/src/main/java/com/jbp/admin/JbpAdminApplication.java
@@ -34,9 +34,6 @@ public class JbpAdminApplication {
         Environment bean = run.getBean(Environment.class);
         System.out.println("spring.datasource.url="+ bean.getProperty("spring.datasource.url"));
         System.out.println("启动完成");
-
-
-
     }
 
 }
--- jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
+++ jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
@@ -49,9 +49,6 @@ public class JbpFrontApplication {
         Environment bean = run.getBean(Environment.class);
         System.out.println("spring.datasource.url=" + bean.getProperty("spring.datasource.url"));
         System.out.println("启动完成");
-
-//        final LogisticService bean1 = run.getBean(LogisticService.class);
-//        bean1.info("621110728192092", null, "", "15871898210");
     }
 
 }
