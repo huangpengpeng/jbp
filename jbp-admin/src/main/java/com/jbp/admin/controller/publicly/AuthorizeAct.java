5f44636a2af91467e52601f99ac963d132c96cb4
huangliuzhong <a15870064803@126.com>
2024-04-19 11:35:56 +0800

    i

--- jbp-admin/src/main/java/com/jbp/admin/controller/publicly/AuthorizeAct.java
+++ jbp-admin/src/main/java/com/jbp/admin/controller/publicly/AuthorizeAct.java
@@ -18,6 +18,7 @@ import com.jbp.service.service.ExpressService;
 import com.jbp.service.service.MerchantOrderService;
 import com.jbp.service.service.OrderDetailService;
 import com.jbp.service.service.OrderService;
+import com.jbp.service.util.StringUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +29,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -77,17 +79,30 @@ public class AuthorizeAct {
 
 	}
 	
-	@ApiOperation(value = "erp 聚水潭 消息推送", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
-	@RequestMapping(value = "/jushuitan/callApi1", produces = MediaType.APPLICATION_JSON_VALUE)
-	public void erpcallApi1(JSONObject jsonObject, HttpServletResponse response) {
-		{
-			log.info("jushuitancallApi:{} jsonObject:{}", JushuitanCallSvc.getLocation(JushuitanCallSvc.getNativeRequest()),
-					jsonObject.toJSONString());
+
+	@ApiOperation(value = "erp 聚水潭 消息推送")
+	@RequestMapping(value = "/jushuitan/callApi1")
+	public void erpcallApi1(String jsonObject,HttpServletResponse response) {
+
+
+		if(StringUtils.isBlank(jsonObject)){
+			try {
+				response.getWriter().write( "{\"code\":\"0\",\"msg\":\"执行成功\"}");
+				return;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		{
-			String orderSn = jsonObject.getString("so_id");
-			String shipName = jsonObject.getString("logistics_company");
-			String shipSn = jsonObject.getString("l_id");
+			log.info("jushuitancallApi:{} jsonObject:{}", JushuitanCallSvc.getLocation(JushuitanCallSvc.getNativeRequest()),
+					jsonObject);
+		}
+			JSONObject jsonObject1  =JSONObject.parseObject(jsonObject);
+
+		{
+			String orderSn = jsonObject1.getString("so_id");
+			String shipName = jsonObject1.getString("logistics_company");
+			String shipSn = jsonObject1.getString("l_id");
 			Order orders = orderService.getByOrderNo(orderSn);
 			if (!orders.getStatus().equals(OrderConstants.ORDER_STATUS_WAIT_SHIPPING)) {
 				throw new RuntimeException(orders.getOrderNo() + "状态错误");
@@ -117,22 +132,33 @@ public class AuthorizeAct {
 			orderSendRequest.setExpressTempId(shipSn);
 			try {
 				orderService.send(orderSendRequest);
-				response.getWriter().write( "{\"code\":\"0\",\"msg\":\"执行成功\"}");
+			response.getWriter().write( "{\"code\":\"0\",\"msg\":\"执行成功\"}");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-
 		}
 
 	}
 	@ApiOperation(value = "erp 聚水潭 消息推送", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/jushuitan/callApi2", produces = MediaType.APPLICATION_JSON_VALUE)
-	public void erpcallApi2(JSONObject jsonObject,HttpServletResponse response) {
+	public void erpcallApi2(String jsonObject,HttpServletResponse response) {
 		{
 			log.info("jushuitancallApi:{}",JushuitanCallSvc.getLocation(JushuitanCallSvc.getNativeRequest()));
 		}
-		String orderSn = jsonObject.getString("so_id");
-		String refundMark= jsonObject.getString("remark");
+
+
+		if(StringUtils.isBlank(jsonObject)){
+			try {
+				response.getWriter().write( "{\"code\":\"0\",\"msg\":\"执行成功\"}");
+				return;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		JSONObject jsonObject1  =JSONObject.parseObject(jsonObject);
+
+		String orderSn = jsonObject1.getString("so_id");
+		String refundMark= jsonObject1.getString("remark");
 
 		Order orders = orderService.getByOrderNo(orderSn);
 		//退款
@@ -145,7 +171,7 @@ public class AuthorizeAct {
 	}
 	@ApiOperation(value = "erp 聚水潭 消息推送", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/jushuitan/callApi3", produces = MediaType.APPLICATION_JSON_VALUE)
-	public void erpcallApi3(HttpServletResponse response, ModelMap model) {
+	public void erpcallApi3(HttpServletResponse response) {
 		{
 			log.info("jushuitancallApi:{}",JushuitanCallSvc.getLocation(JushuitanCallSvc.getNativeRequest()));
 		}
@@ -157,12 +183,21 @@ public class AuthorizeAct {
 	}
 	@ApiOperation(value = "erp 聚水潭 消息推送", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/jushuitan/callApi4", produces = MediaType.APPLICATION_JSON_VALUE)
-	public void erpcallApi4(JSONObject jsonObject,HttpServletResponse response) {
+	public void erpcallApi4( String jsonObject,HttpServletResponse response) {
 		{
 			log.info("jushuitancallApi:{}",JushuitanCallSvc.getLocation(JushuitanCallSvc.getNativeRequest()));
 		}
-		String orderSn = jsonObject.getString("so_id");
-		String refundMark= jsonObject.getString("remark");
+		if(StringUtils.isBlank(jsonObject)){
+			try {
+				response.getWriter().write( "{\"code\":\"0\",\"msg\":\"执行成功\"}");
+				return;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		JSONObject jsonObject1  =JSONObject.parseObject(jsonObject);
+		String orderSn = jsonObject1.getString("so_id");
+		String refundMark= jsonObject1.getString("remark");
 
 		//退款
 //		Orders orders=ordersMng.getByOrderSn(orderSn);
