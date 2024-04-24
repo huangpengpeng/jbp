0a0772d2fe5807f339c413e8fb9ff7353b4240d8
fanyufengkaiying <956838209@qq.com>
2024-04-24 14:19:46 +0800

    1

--- jbp-common/src/main/java/com/jbp/common/utils/CrmebUtil.java
+++ jbp-common/src/main/java/com/jbp/common/utils/CrmebUtil.java
@@ -181,7 +181,7 @@ public class CrmebUtil {
      */
     public static void main(String[] args) throws Exception {
 //       System.out.println(encryptPassword("123456"));
-        System.out.println(decryptPassowrd("0ngdPxVkYyu7WNvMaHACcA==", "18929560630"));
+        System.out.println(decryptPassowrd("EtcZy0MJ4b0E6BQ/geHLLg==", "18216024339"));
     }
 
     /**
