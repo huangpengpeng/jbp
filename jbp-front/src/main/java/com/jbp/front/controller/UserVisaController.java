9a782fd54a595e99d28b8a38b056b97ab2ab34d0
huangliuzhong <a15870064803@126.com>
2024-04-24 11:21:51 +0800

    i

--- jbp-front/src/main/java/com/jbp/front/controller/UserVisaController.java
+++ jbp-front/src/main/java/com/jbp/front/controller/UserVisaController.java
@@ -265,18 +265,18 @@ public class UserVisaController {
             List<AddFillFieldInfo> fillFields = new ArrayList<>();
             AddFillFieldInfo fillField = new AddFillFieldInfo();
             fillField.setFieldDocId("32120153");
-            fillField.setFieldId("9841172227");
+            fillField.setFieldId("0884951046");
             fillField.setFieldValue(userViseSaveRequest.getIdCard());
             fillFields.add(fillField);
 
             AddFillFieldInfo fillField2 = new AddFillFieldInfo();
             fillField2.setFieldDocId("32120153");
-            fillField2.setFieldId("2385685580");
+            fillField2.setFieldId("2740983788");
             fillField2.setFieldValue(userViseSaveRequest.getOrderNo());
             fillFields.add(fillField2);
             AddFillFieldInfo fillField3 = new AddFillFieldInfo();
             fillField3.setFieldDocId("32120153");
-            fillField3.setFieldId("9669820250");
+            fillField3.setFieldId("1976441880");
             fillField3.setFieldValue(userId.toString());
             fillFields.add(fillField3);
 
