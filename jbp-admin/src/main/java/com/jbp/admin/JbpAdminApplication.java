bce0b9b2339c3b51599532425f74d3833def603b
huangliuzhong <a15870064803@126.com>
2024-04-26 14:31:43 +0800

    i

--- jbp-admin/src/main/java/com/jbp/admin/JbpAdminApplication.java
+++ jbp-admin/src/main/java/com/jbp/admin/JbpAdminApplication.java
@@ -37,12 +37,12 @@ public class JbpAdminApplication {
         System.out.println("spring.datasource.url="+ bean.getProperty("spring.datasource.url"));
         System.out.println("启动完成");
 
-
-        TankStoreRelationAct groupThreeRetOneHandler = run.getBean(TankStoreRelationAct.class);
-        groupThreeRetOneHandler.aa_3968();
-        groupThreeRetOneHandler.aa_15866();
-        groupThreeRetOneHandler.aa_3174_4();
-        groupThreeRetOneHandler.aa_12692_8();
+//
+//        TankStoreRelationAct groupThreeRetOneHandler = run.getBean(TankStoreRelationAct.class);
+//        groupThreeRetOneHandler.aa_3968();
+//        groupThreeRetOneHandler.aa_15866();
+//        groupThreeRetOneHandler.aa_3174_4();
+//        groupThreeRetOneHandler.aa_12692_8();
 
 //        OrderService orderService = run.getBean(OrderService.class);
 //
