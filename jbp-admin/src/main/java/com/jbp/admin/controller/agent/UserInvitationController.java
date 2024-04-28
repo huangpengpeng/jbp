4d84546e9b24fa3f59a84d667ddb0e3bf7f062f4
huangliuzhong <a15870064803@126.com>
2024-04-28 19:25:41 +0800

    i

--- jbp-admin/src/main/java/com/jbp/admin/controller/agent/UserInvitationController.java
+++ jbp-admin/src/main/java/com/jbp/admin/controller/agent/UserInvitationController.java
@@ -122,7 +122,7 @@ public class UserInvitationController {
 
         Integer pid =  userInvitationService.getPid(user.getId());
 
-        if (userInvitationService.hasChild(pUser.getId(), pid)) {
+        if (userInvitationService.hasChild(pid ,pUser.getId())) {
             throw new RuntimeException("关系链条的上级不能绑定给自己绑定账户");
         }
 
