e7f642bb69ff8c3235254becbb189fb26399e0be
huangliuzhong <a15870064803@126.com>
2024-04-29 11:07:10 +0800

    i

--- jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
+++ jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
@@ -1,7 +1,13 @@
 package com.jbp.front;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.binarywang.spring.starter.wxjava.miniapp.config.WxMaAutoConfiguration;
+import com.jbp.common.result.CommonResult;
+import com.jbp.front.controller.tank.TankAct;
 import com.jbp.service.service.agent.WalletFlowService;
+import jodd.http.HttpRequest;
+import jodd.http.HttpResponse;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +19,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * 程序主入口
@@ -39,6 +48,37 @@ public class JbpFrontApplication {
         Environment bean = run.getBean(Environment.class);
         System.out.println("spring.datasource.url=" + bean.getProperty("spring.datasource.url"));
         System.out.println("启动完成");
+
+
+//        Map<String, String> params = new TreeMap<String, String>();
+//
+//        HttpRequest request = HttpRequest.post("https://system.swgzsb.com/api/external_service/user/login");
+//        request.contentType("application/json");
+//        request.charset("utf-8");
+//
+//        params.put("appid", "2t7VHlqR0vZn5x1EijFYJom8SKkIDTbc");
+//        params.put("secret", "uhQgSRoyM3FifxwnpDatO7TAUP2Nmd1J");
+//        request.body(JSON.toJSONString(params));
+//        HttpResponse response1 = request.send();
+//        String respJson = response1.bodyText();
+//        JSONObject goodsJson = JSONObject.parseObject(respJson).getJSONObject("data");
+//
+//
+//
+//        Map<String, Object> params2 = new TreeMap<String, Object>();
+//        HttpRequest request2 = HttpRequest.post("https://system.swgzsb.com/api/external_service/config/updateEquipmentQrcodeRule");
+//        request2.contentType("application/json");
+//        request2.charset("utf-8");
+//
+//      //  String userPrefix = environment.getProperty("gxc.url");
+//        params2.put("token",goodsJson.get("token"));
+//        params2.put("equipmentQrcodeRule", "https://sm.fnyss.cc/equipment_id/?equipmentSn={$equipment_id}");
+//        request2.body(JSON.toJSONString(params2));
+//        HttpResponse response3 = request2.send();
+//        String respJson3 = response3.bodyText();
+//        JSONObject goodsJson3 = JSONObject.parseObject(respJson3).getJSONObject("data");
+////        return CommonResult.success(goodsJson);
+
 
 
     }
