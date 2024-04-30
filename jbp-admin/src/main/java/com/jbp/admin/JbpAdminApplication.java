b2a4801fc78809563a447fa9b9838cb0ccc37b83
fanyufengkaiying <956838209@qq.com>
2024-04-30 14:02:10 +0800

    1

--- jbp-admin/src/main/java/com/jbp/admin/JbpAdminApplication.java
+++ jbp-admin/src/main/java/com/jbp/admin/JbpAdminApplication.java
@@ -1,6 +1,8 @@
 package com.jbp.admin;
 
 import com.binarywang.spring.starter.wxjava.miniapp.config.WxMaAutoConfiguration;
+import com.jbp.common.model.agent.LztFundTransfer;
+import com.jbp.service.service.agent.LztFundTransferService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.math.BigDecimal;
 
 
 @EnableAsync //开启异步调用	
@@ -26,6 +30,10 @@ public class JbpAdminApplication {
         ConfigurableApplicationContext run = SpringApplication.run(JbpAdminApplication.class, args);
         System.out.println("ok");
 
+        LztFundTransferService lztFundTransferService = run.getBean(LztFundTransferService.class);
+
+        LztFundTransfer lztFundTransfer = lztFundTransferService.fundTransfer(14, "d0002", "80094800000009870000000010", BigDecimal.valueOf(3000000), "付款");
+        System.out.println(lztFundTransfer);
     }
 
 }
