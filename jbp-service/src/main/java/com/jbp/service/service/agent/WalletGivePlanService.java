5320cf57c4a52575cdbc59dbb856f7d1b8b98cc0
yaozhuqing <1085445367@qq.com>
2024-05-07 19:30:33 +0800

    3

--- jbp-service/src/main/java/com/jbp/service/service/agent/WalletGivePlanService.java
+++ jbp-service/src/main/java/com/jbp/service/service/agent/WalletGivePlanService.java
@@ -1,9 +1,12 @@
 package com.jbp.service.service.agent;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.pagehelper.PageInfo;
 import com.jbp.common.model.agent.WalletConfig;
 import com.jbp.common.model.agent.WalletGivePlan;
 import com.jbp.common.model.user.User;
+import com.jbp.common.request.PageParamRequest;
+import com.jbp.common.request.agent.WalletGivePlanRequest;
 
 import java.math.BigDecimal;
 
@@ -13,5 +16,7 @@ public interface WalletGivePlanService extends IService<WalletGivePlan> {
     void release();
 
     void cancel(String externalNo);
+
+    PageInfo<WalletGivePlan> pageList(WalletGivePlanRequest request, PageParamRequest pageParamRequest);
 
 }
--- jbp-service/src/main/java/com/jbp/service/service/agent/impl/WalletGivePlanServiceImpl.java
+++ jbp-service/src/main/java/com/jbp/service/service/agent/impl/WalletGivePlanServiceImpl.java
@@ -1,11 +1,18 @@
 package com.jbp.service.service.agent.impl;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jbp.common.model.agent.WalletConfig;
 import com.jbp.common.model.agent.WalletFlow;
 import com.jbp.common.model.agent.WalletGivePlan;
 import com.jbp.common.model.user.User;
 import com.jbp.common.mybatis.UnifiedServiceImpl;
+import com.jbp.common.page.CommonPage;
+import com.jbp.common.request.PageParamRequest;
+import com.jbp.common.request.agent.WalletGivePlanRequest;
 import com.jbp.common.utils.DateTimeUtils;
 import com.jbp.service.dao.agent.WalletGivePlanDao;
 import com.jbp.service.service.agent.PlatformWalletService;
@@ -69,6 +76,18 @@ public class WalletGivePlanServiceImpl extends UnifiedServiceImpl<WalletGivePlan
             plan.setStatus("已取消");
             updateById(plan);
         }
+    }
+
+    @Override
+    public PageInfo<WalletGivePlan> pageList(WalletGivePlanRequest request, PageParamRequest pageParamRequest) {
+        LambdaQueryWrapper<WalletGivePlan> lqw = new LambdaQueryWrapper<WalletGivePlan>()
+                .eq(!ObjectUtil.isNull(request.getUid()), WalletGivePlan::getUid, request.getUid())
+                .eq(!ObjectUtil.isNull(request.getWalletType()), WalletGivePlan::getWalletType, request.getWalletType())
+                .eq(!ObjectUtil.isEmpty(request.getAccount()), WalletGivePlan::getAccount, request.getAccount())
+                .eq(!ObjectUtil.isEmpty(request.getExternalNo()), WalletGivePlan::getExternalNo, request.getExternalNo());
+        Page<WalletGivePlan> page = PageHelper.startPage(pageParamRequest.getPage(), pageParamRequest.getLimit());
+        List<WalletGivePlan> list = list(lqw);
+            return CommonPage.copyPageInfo(page, list);
     }
 
 }
