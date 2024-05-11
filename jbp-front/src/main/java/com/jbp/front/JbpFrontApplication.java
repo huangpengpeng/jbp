e87156f1fed7d25c868be6a3ff6a937610f4c4bc
fanyufengkaiying <956838209@qq.com>
2024-05-11 12:01:54 +0800

    1

--- jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
+++ jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
@@ -1,9 +1,6 @@
 package com.jbp.front;
 
 import com.binarywang.spring.starter.wxjava.miniapp.config.WxMaAutoConfiguration;
-import com.jbp.common.lianlian.result.CashierPayCreateResult;
-import com.jbp.common.utils.StringUtils;
-import com.jbp.service.service.LianLianPayService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +12,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.math.BigDecimal;
 
 /**
  * 程序主入口
@@ -43,15 +38,5 @@ public class JbpFrontApplication {
         Environment bean = run.getBean(Environment.class);
         System.out.println("spring.datasource.url=" + bean.getProperty("spring.datasource.url"));
         System.out.println("启动完成");
-
-        LianLianPayService lianLianPayService = run.getBean(LianLianPayService.class);
-
-        // String account, String payCode, BigDecimal amount, String goodsName, String ip
-
-        String payCode = StringUtils.N_TO_10("CS_");
-        System.out.println(payCode);
-        CashierPayCreateResult cashier = lianLianPayService.cashier("franky", payCode, BigDecimal.ONE, "测试", "125.119.68.138");
-        System.out.println("调用完成");
-
     }
 }
