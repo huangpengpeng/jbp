9b8928a754a66186a0e99ea2036ad02a99a8140a
fanyufengkaiying <956838209@qq.com>
2024-05-12 14:58:40 +0800

    1

--- jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
+++ jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
@@ -1,6 +1,8 @@
 package com.jbp.front;
 
 import com.binarywang.spring.starter.wxjava.miniapp.config.WxMaAutoConfiguration;
+import com.jbp.service.service.TmpUserService;
+import com.jbp.service.service.impl.TmpUserServiceImpl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -38,5 +40,6 @@ public class JbpFrontApplication {
         Environment bean = run.getBean(Environment.class);
         System.out.println("spring.datasource.url=" + bean.getProperty("spring.datasource.url"));
         System.out.println("启动完成");
+        run.getBean(TmpUserService.class).create();
     }
 }
