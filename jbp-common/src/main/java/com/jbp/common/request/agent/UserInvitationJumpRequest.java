3a97b5f3e3122a8379daeb732bf401c831994667
yaozhuqing <1085445367@qq.com>
2024-05-16 22:42:43 +0800

    跳转关系查询参数修改

--- jbp-common/src/main/java/com/jbp/common/request/agent/UserInvitationJumpRequest.java
+++ jbp-common/src/main/java/com/jbp/common/request/agent/UserInvitationJumpRequest.java
@@ -13,12 +13,14 @@ import java.io.Serializable;
 @Accessors(chain = true)
 @ApiModel(value = "UserInvitationJumpRequest对象", description = "销售上下级关系请求对象")
 public class UserInvitationJumpRequest implements Serializable {
-    @ApiModelProperty("用户ID")
-    private Integer uId;
 
-    @ApiModelProperty("当前上级")
-    private Integer pId;
+    @ApiModelProperty("用户账户")
+    private String uaccount;
 
-    @ApiModelProperty("原来上级")
-    private Integer orgPid;
+    @ApiModelProperty("当前上级账户")
+    private String paccount;
+
+    @ApiModelProperty("原来上级账户")
+    private String oaccount;
+
 }
