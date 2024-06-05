a1899a09ba737983d4a7df39c323dcb0d0b52476
fanyufengkaiying <956838209@qq.com>
2024-06-05 21:43:55 +0800

    1

--- jbp-admin/src/main/java/com/jbp/admin/controller/publicly/AuthorizeAct.java
+++ jbp-admin/src/main/java/com/jbp/admin/controller/publicly/AuthorizeAct.java
@@ -1,24 +1,11 @@
 package com.jbp.admin.controller.publicly;
 
-import com.alibaba.fastjson.JSONObject;
 import com.beust.jcommander.internal.Maps;
-import com.jbp.common.constants.OrderConstants;
-import com.jbp.common.model.express.Express;
-import com.jbp.common.model.order.MerchantOrder;
-import com.jbp.common.model.order.Order;
-import com.jbp.common.model.order.OrderDetail;
-import com.jbp.common.request.OrderSendRequest;
-import com.jbp.common.request.SplitOrderSendDetailRequest;
+import com.jbp.common.encryptapi.EncryptIgnore;
 import com.jbp.common.result.CommonResult;
-import com.jbp.service.erp.service.JushuitanCallSvc;
 import com.jbp.service.erp.service.JushuitanOrderSvc;
 import com.jbp.service.erp.tools.Constants;
 import com.jbp.service.erp.tools.SignUtil;
-import com.jbp.service.service.ExpressService;
-import com.jbp.service.service.MerchantOrderService;
-import com.jbp.service.service.OrderDetailService;
-import com.jbp.service.service.OrderService;
-import com.jbp.service.util.StringUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -26,18 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import springfox.documentation.annotations.ApiIgnore;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -47,58 +25,42 @@ import java.util.Map;
 @EncryptIgnore
 public class AuthorizeAct {
 
+    @Autowired
+    private Environment environment;
+    @Autowired
+    private JushuitanOrderSvc jushuitanOrderSvc;
 
 
-	@ApiOperation(value = "授权订单", produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	@RequestMapping(value = { "/sync2" }, produces = MediaType.APPLICATION_JSON_VALUE)
-	public CommonResult<String>  sync2() {
-		Map<String, Object> params = Maps.newHashMap();
-		params.put("app_key",  environment.getProperty("jushuitan.appKey"));
-	    params.put("timestamp", String.valueOf(System.currentTimeMillis() / 1000));
-		params.put("charset", Constants.CHARSET);
-		String state = environment.getProperty("jushuitan.url");
-		params.put("state", state);
-		String sign = SignUtil.getSign(environment.getProperty("jushuitan.appSecret"), params);
-		/**
-		 * 拼接跳转地址
-		 */
-		String url = Constants.AUTH_URL.replace("[app_key]", (String)params.get("app_key"))
-				.replace("[timestamp]", (String)params.get("timestamp")).replace("[charset]", (String)params.get("charset"))
-				.replace("[sign]", sign).replace("[state]", state);
-	//	return "redirect:"+url;
-		return CommonResult.success(url);
+    @ApiOperation(value = "授权订单", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @RequestMapping(value = {"/sync2"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResult<String> sync2() {
+        Map<String, Object> params = Maps.newHashMap();
+        params.put("app_key", environment.getProperty("jushuitan.appKey"));
+        params.put("timestamp", String.valueOf(System.currentTimeMillis() / 1000));
+        params.put("charset", Constants.CHARSET);
+        String state = environment.getProperty("jushuitan.url");
+        params.put("state", state);
+        String sign = SignUtil.getSign(environment.getProperty("jushuitan.appSecret"), params);
+        /**
+         * 拼接跳转地址
+         */
+        String url = Constants.AUTH_URL.replace("[app_key]", (String) params.get("app_key"))
+                .replace("[timestamp]", (String) params.get("timestamp")).replace("[charset]", (String) params.get("charset"))
+                .replace("[sign]", sign).replace("[state]", state);
+        //	return "redirect:"+url;
+        return CommonResult.success(url);
 
-	}
-
-
+    }
 
 
+    @ApiOperation(value = "同步订单", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @RequestMapping(value = {"/sync"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResult<String> sync() {
+        jushuitanOrderSvc.sync("");
+        return CommonResult.success("同步成功");
+    }
 
-	@ApiOperation(value = "同步订单", produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	@RequestMapping(value = { "/sync" }, produces = MediaType.APPLICATION_JSON_VALUE)
-	public CommonResult<String> sync() {
-		jushuitanOrderSvc.sync("");
-		return CommonResult.success("同步成功");
-	}
-
-
-
-
-	@Autowired
-	private OrderDetailService orderDetailService;
-	@Autowired
-	private MerchantOrderService merchantOrderService;
-	@Autowired
-	private OrderService orderService;
-	@Autowired
-	private JushuitanCallSvc callSvc;
-	@Autowired
-	private Environment environment;
-	@Autowired
-	private ExpressService expressService;
-	@Autowired
-	private JushuitanOrderSvc jushuitanOrderSvc;
 
 }
