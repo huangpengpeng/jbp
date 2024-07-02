1ef743250033286c4bef943af842f145fe5434b2
huangliuzhong <a15870064803@126.com>
2024-07-02 18:16:41 +0800

    i

--- jbp-admin/src/main/java/com/jbp/admin/JbpAdminApplication.java
+++ jbp-admin/src/main/java/com/jbp/admin/JbpAdminApplication.java
@@ -2,6 +2,12 @@ package com.jbp.admin;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.binarywang.spring.starter.wxjava.miniapp.config.WxMaAutoConfiguration;
+import com.jbp.common.model.order.Order;
+import com.jbp.common.model.order.OrderDetail;
+import com.jbp.service.product.comm.CommCalculateResult;
+import com.jbp.service.product.comm.ProductCommChain;
+import com.jbp.service.service.OrderDetailService;
+import com.jbp.service.service.OrderService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -34,15 +40,22 @@ public class JbpAdminApplication {
         System.out.println("启动完成");
 
 
-//        ProductCommChain productCommChain = run.getBean(ProductCommChain.class);
-//       OrderDetailService orderDetailService = run.getBean(OrderDetailService.class);
-//       OrderService orderService = run.getBean(OrderService.class);
-//     Order order  = orderService.getOne(new QueryWrapper<Order>().lambda().eq(Order::getOrderNo,"PT826171989041490571007"));
-////
-//       List<OrderDetail> platOrderDetailList = orderDetailService.getByOrderNo(order.getOrderNo());
-//         LinkedList<CommCalculateResult> commList = new LinkedList<>();
-//////
-//        productCommChain.orderSuccessCalculateAmt(order,platOrderDetailList,commList);
+        ProductCommChain productCommChain = run.getBean(ProductCommChain.class);
+       OrderDetailService orderDetailService = run.getBean(OrderDetailService.class);
+       OrderService orderService = run.getBean(OrderService.class);
+     List<Order> orderList  = orderService.list(new QueryWrapper<Order>().lambda().last( " where order_no in ('PT431171855033283233516',\n" +
+             "'PT838171871165760286922',\n" +
+             "'PT752171871193261926697',  \n" +
+             "'PT395171886316622891339',  \n" +
+             "'PT301171888526886374487')" ));
+
+     for(Order order : orderList){
+         List<OrderDetail> platOrderDetailList = orderDetailService.getByOrderNo(order.getOrderNo());
+         LinkedList<CommCalculateResult> commList = new LinkedList<>();
+
+         productCommChain.orderSuccessCalculateAmt(order,platOrderDetailList,commList);
+     }
+
 
 
     }
