07d0a05c20e74fb40c58afe533aacaaad89ff39e
huangliuzhong <a15870064803@126.com>
2024-07-03 01:16:49 +0800

    i

--- jbp-service/src/main/java/com/jbp/service/product/comm/WhiteGroupThreeRetOneHandler.java
+++ jbp-service/src/main/java/com/jbp/service/product/comm/WhiteGroupThreeRetOneHandler.java
@@ -136,7 +136,7 @@ public class WhiteGroupThreeRetOneHandler extends AbstractProductCommHandler {
             Integer orderSize = orderDetailService.getNextOrderGoods(pid, map.get("product_id").toString(), userCapa.getCapaId(),rule.getAmt().toString());
 
             //计算当前笔数
-            orderSize = orderSize + 1;
+            orderSize = orderSize ;
 
             int remainder = orderSize % rule.getGroupComm().size();
 
