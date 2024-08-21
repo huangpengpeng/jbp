b089bd5dc68e98d3f76b68ec5d8c36da46017ea8
huangliuzhong <a15870064803@126.com>
2024-08-21 17:43:33 +0800

    i

--- jbp-service/src/main/java/com/jbp/service/service/agent/impl/ClearingUserServiceImpl.java
+++ jbp-service/src/main/java/com/jbp/service/service/agent/impl/ClearingUserServiceImpl.java
@@ -249,7 +249,7 @@ public class ClearingUserServiceImpl extends UnifiedServiceImpl<ClearingUserDao,
                 throw new CrmebException("月度管理补贴规则为空请联系管理员");
             }
             Long capaId = ruleMap.values().stream().collect(Collectors.toList()).get(0).getCapaId();
-            List<UserCapa> userCapaList = userCapaService.list(new LambdaQueryWrapper<UserCapa>().eq(UserCapa::getCapaId, capaId));
+            List<UserCapa> userCapaList = userCapaService.list(new LambdaQueryWrapper<UserCapa>().last( "where capa_id in (18,19)"));
             if (CollectionUtils.isEmpty(userCapaList)) {
                 return;
             }
