1f818c3b0386ab08dd038d4d6bdae33fd8226e36
fanyufengkaiying <956838209@qq.com>
2024-09-23 14:39:01 +0800

    1

--- jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
+++ jbp-front/src/main/java/com/jbp/front/JbpFrontApplication.java
@@ -1,6 +1,7 @@
 package com.jbp.front;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.binarywang.spring.starter.wxjava.miniapp.config.WxMaAutoConfiguration;
 import com.jbp.common.kqbill.result.KqPayQueryResult;
 import com.jbp.service.service.KqPayService;
@@ -44,6 +45,10 @@ import java.util.Date;
         Environment bean = run.getBean(Environment.class);
         System.out.println("spring.datasource.url=" + bean.getProperty("spring.datasource.url"));
         System.out.println("启动完成");
+
+        KqPayService kqPayService = run.getBean(KqPayService.class);
+        KqPayQueryResult result = kqPayService.queryPayResult("PT985172706701730859627");
+        System.out.println(JSONObject.toJSONString(result));
 
 
 
